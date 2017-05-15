de9bde9138e34d81f4997f0821a5a63c5afb8bbe
Prajna Peters <prajnacheryl@yahoo.co.in>
2017-05-15 16:39:58 -0400

    First draft

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function setCopyrightYear($selector) {
+  var d = new Date();
+  
+  
+  $selector.text(d.getFullYear());
+}
+
+$('.js-scroll').on('click', function(event) {
+  event.preventDefault();
+  var target = $(this).data('scrollTo');
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, 400);
+});
+
+setCopyrightYear($('#copyright-year'));
