4edf44024fba95f5af600d1e930ddaaffe95b394
Sneha Tilak <sneha.tilak26@gmail.com>
2017-06-18 17:59:33 -0400

    Changes

---
+++ php/send_form_email.php
@@ -0,0 +1,66 @@
+<?php 
+
+// define variables and set to empty values
+$name_error = $email_error = $phone_error = $url_error = "";
+$name = $email = $phone = $message = $url = $success = "";
+
+//form is submitted with POST method
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $name_error = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $name_error = "Only letters and white space allowed"; 
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $email_error = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $email_error = "Invalid email format"; 
+    }
+  }
+
+  if (empty($_POST["message"])) {
+    $message = "";
+  } else {
+    $message = test_input($_POST["message"]);
+  }
+  
+  if ($name_error == '' and $email_error == '' and $phone_error == '' and $url_error == '' ){
+      $message_body = '';
+      unset($_POST['submit']);
+      foreach ($_POST as $key => $value){
+          $message_body .=  "$key: $value\n";
+      }
+
+      $headers =  'MIME-Version: 1.0' . "\r\n"; 
+      $headers .= 'From: Your name <info@address.com>' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+      //mail($to, $subject, $body, $headers);
+      
+      $to = 'prajnacheryl@yahoo.co.in';
+      $from = $email;
+      $subject = 'Contact Form Submit';
+      if (mail($to, $subject, $message, $headers)){
+          $success = "Message sent, thank you for contacting us!";
+          $name = $email = $phone = $message = $url = '';
+      }
+  }
+  
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
